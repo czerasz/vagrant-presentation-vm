e17b58ada1377fd83ea05563d702458eb3cbe817
Michał Czeraszkiewicz <czerasz@gmail.com>
2014-02-21 12:23:47 +0100

    Add manifest example with Facter and file templates

--- shared/modules/hello_world/manifests/init.pp
+++ shared/modules/hello_world/manifests/init.pp
@@ -1,1 +1,16 @@
-notify { 'Hello World': }
+# Run with:
+# sudo puppet apply /home/vagrant/htdocs/modules/hello_world/manifests/init.pp --debug --modulepath=/home/vagrant/htdocs/modules/
+
+if $::operatingsystem == 'Ubuntu' {
+    file { '/etc/motd':
+      path    => '/etc/motd',
+      content => template('hello_world/etc/motd_ubuntu.erb'),
+      backup  => '.puppet-bak'
+    }
+} else {
+    file { '/etc/motd':
+      path    => '/etc/motd',
+      content => template('hello_world/etc/motd_other.erb'),
+      backup  => '.puppet-bak'
+    }
+}
